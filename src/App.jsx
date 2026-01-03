2380606c6409055bc6f5e5b407cba77f33dc0dcb
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-01-03 21:08:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import './App.css';
+import './mobile-fix.css';
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
@@ -384,3 +384,4 @@ function App() {
 }
 
 export default App;
+
